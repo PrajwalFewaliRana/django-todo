125a716c42b559d84642fecce810a93c8966acf0
PrajwalFewaliRana <prajwalmagar68@gmail.com>
2025-12-11 22:12:04 +0545

    todo functions

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,25 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404, redirect, render
+from .models import Task
 # Create your views here.
 
 def addTask(request):
-    return HttpResponse('The form is submitted')
+    task=request.POST['task']
+    Task.objects.create(task=task)
+    return redirect('home')
+
+
+def mark_as_done(request,pk):
+    task = get_object_or_404(Task,pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
+
+def mark_as_undone(request,pk):
+    task = get_object_or_404(Task,pk=pk)
+    task.is_completed=False
+    task.save()
+    return redirect('home')
+
+def edit_task(request,pk):
+    return render(request,'edit_task.html')
